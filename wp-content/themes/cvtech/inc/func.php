b574d49b6bd7e52ec2814e9e95150db484635fac
QVan27 <qvannarath@gmail.com>
2021-02-08 15:25:07 +0100

    prépa wp

---
+++ wp-content/themes/cvtech/inc/func.php
@@ -0,0 +1,145 @@
+<?php 
+
+/**
+*  print_r coké
+*  @param mixed $var La variable à déboger
+*/
+
+function debug($var)
+{
+    echo '<pre style="height:200px;overflow-y: scroll;font-size:.8em;padding: 10px; font-family: Consolas, Monospace; background-color: #000; color: #fff;">';
+    print_r($var);
+    echo '</pre>';
+}
+
+
+
+// Fonction FORMULAIRE 
+
+function ValidationText($errors,$data,$key,$min,$max)
+{
+  if(!empty($data)) {
+    if(mb_strlen($data) < $min) {
+      $errors[$key] = 'Min '.$min.' caractères';
+    } elseif(mb_strlen($data) > $max) {
+      $errors[$key] = 'Max '.$max.' caractères';
+    }
+  } else {
+    $errors[$key] = 'Veuillez renseigner ce champ';
+  }
+  return $errors;
+}
+
+function cleanXss($value)
+{
+  return trim(strip_tags($value));
+}
+
+function emailValidation($err,$mail,$key)
+{
+    if(!empty($mail)) {
+        if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+            $err[$key] = 'Email non valide';
+        }
+    } else {
+        $err[$key] = 'Veuillez renseigner ce champ';
+    }
+    return $err;
+}
+
+//////////// FIN DE FONCTION FORMULAIRE ///////////////////
+
+//////////// PAGINATION
+
+function pagination($pages = '', $range = 4)
+{  
+     $showitems = ($range * 2)+1;  
+ 
+     global $paged;
+     if(empty($paged)) $paged = 1;
+ 
+     if($pages == '')
+     {
+         global $wp_query;
+         $pages = $wp_query->max_num_pages;
+         if(!$pages)
+         {
+             $pages = 1;
+         }
+     }   
+ 
+     if(1 != $pages)
+     {
+         echo "<div class=\"pagination\"><span>Page ".$paged." of ".$pages."</span>";
+         if($paged > 2 && $paged > $range+1 && $showitems < $pages) echo "<a href='".get_pagenum_link(1)."'>&laquo; First</a>";
+         if($paged > 1 && $showitems < $pages) echo "<a href='".get_pagenum_link($paged - 1)."'>&lsaquo; Previous</a>";
+ 
+         for ($i=1; $i <= $pages; $i++)
+         {
+             if (1 != $pages &&( !($i >= $paged+$range+1 || $i <= $paged-$range-1) || $pages <= $showitems ))
+             {
+                 echo ($paged == $i)? "<span class=\"current\">".$i."</span>":"<a href='".get_pagenum_link($i)."' class=\"inactive\">".$i."</a>";
+             }
+         }
+ 
+         if ($paged < $pages && $showitems < $pages) echo "<a href=\"".get_pagenum_link($paged + 1)."\">Next &rsaquo;</a>";  
+         if ($paged < $pages-1 &&  $paged+$range-1 < $pages && $showitems < $pages) echo "<a href='".get_pagenum_link($pages)."'>Last &raquo;</a>";
+         echo "</div>\n";
+     }
+}
+
+function kriesi_pagination($pages = '', $range = 2)
+{  
+     $showitems = ($range * 2)+1;  
+
+     global $paged;
+     if(empty($paged)) $paged = 1;
+
+     if($pages == '')
+     {
+         global $wp_query;
+         $pages = $wp_query->max_num_pages;
+         if(!$pages)
+         {
+             $pages = 1;
+         }
+     }   
+
+     if(1 != $pages)
+     {
+         echo "<div class='pagination'>";
+         if($paged > 2 && $paged > $range+1 && $showitems < $pages) echo "<a href='".get_pagenum_link(1)."'>&laquo;</a>";
+         if($paged > 1 && $showitems < $pages) echo "<a href='".get_pagenum_link($paged - 1)."'>&lsaquo;</a>";
+
+         for ($i=1; $i <= $pages; $i++)
+         {
+             if (1 != $pages &&( !($i >= $paged+$range+1 || $i <= $paged-$range-1) || $pages <= $showitems ))
+             {
+                 echo ($paged == $i)? "<span class='current'>".$i."</span>":"<a href='".get_pagenum_link($i)."' class='inactive' >".$i."</a>";
+             }
+         }
+
+         if ($paged < $pages && $showitems < $pages) echo "<a href='".get_pagenum_link($paged + 1)."'>&rsaquo;</a>";  
+         if ($paged < $pages-1 &&  $paged+$range-1 < $pages && $showitems < $pages) echo "<a href='".get_pagenum_link($pages)."'>&raquo;</a>";
+         echo "</div>\n";
+     }
+}
+
+function pagination_bar( $custom_query ) {
+
+    $total_pages = $custom_query->max_num_pages;
+    $big = 999999999; // need an unlikely integer
+
+    if ($total_pages > 1){
+        $current_page = max(1, get_query_var('paged'));
+
+        echo paginate_links(array(
+            'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+            'format' => '?paged=%#%',
+            'current' => $current_page,
+            'total' => $total_pages,
+        ));
+    }
+}
+
+/////////////// FIN DE PAGINATION //////////////////
---
+++ wp-content/themes/cvtech/inc/general.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * cvtech functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package cvtech
+ */
+if ( ! defined( '_S_VERSION' ) ) {
+	// Replace the version number of the theme on each release.
+	define( '_S_VERSION', '1.0.0' );
+}
+if ( ! function_exists( 'cvtech_setup' ) ) :
+	function cvtech_setup() {
+		load_theme_textdomain( 'cvtech', get_template_directory() . '/languages' );
+		add_theme_support( 'automatic-feed-links' );
+		add_theme_support( 'title-tag' );
+		add_theme_support( 'post-thumbnails' );
+		register_nav_menus(
+			array(
+				'menu-1' => esc_html__( 'Primary', 'cvtech' ),
+			)
+		);
+		add_theme_support( 'customize-selective-refresh-widgets' );	
+	}
+endif;
+add_action( 'after_setup_theme', 'cvtech_setup' );
+/**
+ * Set the content width in pixels, based on the theme's design and stylesheet.
+ *
+ * Priority 0 to make it available to lower priority callbacks.
+ *
+ * @global int $content_width
+ */
+function cvtech_content_width() {
+	$GLOBALS['content_width'] = apply_filters( 'cvtech_content_width', 640 );
+}
+add_action( 'after_setup_theme', 'cvtech_content_width', 0 );
+
+/**
+ * Register widget area.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ */
+function cvtech_widgets_init() {
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Sidebar', 'cvtech' ),
+			'id'            => 'sidebar-1',
+			'description'   => esc_html__( 'Add widgets here.', 'cvtech' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+}
+add_action( 'widgets_init', 'cvtech_widgets_init' );
+/**
+ * Enqueue scripts and styles.
+ */
+function cvtech_scripts() {
+	wp_enqueue_style( 'cvtech-style', get_stylesheet_uri(), array(), _S_VERSION );
+	wp_style_add_data( 'cvtech-style', 'rtl', 'replace' );
+}
+add_action( 'wp_enqueue_scripts', 'cvtech_scripts' );
