d29d4588bf979cf9043cc1033e584cb1c798e8fc
QVan27 <qvannarath@gmail.com>
2021-02-08 15:03:57 +0100

    clean wp blank

--- wp-content/themes/cvtech/footer.php
+++ wp-content/themes/cvtech/footer.php
@@ -10,22 +10,7 @@
  */
 
 ?>
-
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'cvtech' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'cvtech' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'cvtech' ), 'cvtech', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+<!-- footer -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
