edf0018f7de80cde0ba1c72e329def6410ac8f54
QVan27 <qvannarath@gmail.com>
2021-02-09 17:00:26 +0100

    header + banner

--- wp-content/themes/cvtech/index.php
+++ wp-content/themes/cvtech/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The main template file
  *
@@ -15,6 +16,31 @@
 get_header();
 ?>
 
+<img src="wp-content/themes/cvtech/asset/img/blobs1.svg" alt="" class="blobs1">
+<img src="wp-content/themes/cvtech/asset/img/blobs2.svg" alt="" class="blobs2">
+<!-- <div class="dashboard-img"> -->
+    <!-- <img src="wp-content/themes/cvtech/asset/img/img-banner-1.svg" alt="">
+    <img src="wp-content/themes/cvtech/asset/img/img-banner-2.svg" alt="">
+    <img src="wp-content/themes/cvtech/asset/img/img-banner-3.svg" alt=""> -->
+    <img src="" alt="">
+<!-- </div> -->
+<section class="banner">
+    <div class="info-banner">
+    <img src="wp-content/themes/cvtech/asset/img/img-banner-1.svg" alt="" class="vector first">
+    <img src="wp-content/themes/cvtech/asset/img/img-banner-2.svg" alt="" class="vector second">
+    <img src="wp-content/themes/cvtech/asset/img/img-banner-3.svg" alt="" class="vector third">
+    <img src="wp-content/themes/cvtech/asset/img/search.svg" alt="" class="search">
+    <img src="wp-content/themes/cvtech/asset/img/menu-img-banner.svg" alt="" class="vector fourth">
+    <img src="wp-content/themes/cvtech/asset/img/img-banner-5.svg" alt="" class="vector fifth">
+        <div class="text-info">
+            
+            <h1>Créateur de CV en ligne</h1>
+            <p>Choisissez l’un de nos modèles de CV Modifiez votre CV depuis n’importe quel appareil Suivez les exemples et conseils de nos experts Téléchargez-le en PDF ou TXT ou partagez-le en ligne.</p>
+            <a href="#" class="btn-banner">explorer nos services</a>
+        </div>
+        <div class="img-info"><img src="wp-content/themes/cvtech/asset/img/undraw_cv.svg" alt=""></div>
+    </div>
+</section>
 
 
 
@@ -45,7 +71,6 @@ get_header();
 
 
 
-	
 
 
 
