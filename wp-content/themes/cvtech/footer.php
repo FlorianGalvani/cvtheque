40343022b514589535bee30d94d3fdede86cb9d6
yohannmts <yohannmats@gmail.com>
2021-02-10 11:30:46 +0100

    ajout du footer et de son style

--- wp-content/themes/cvtech/footer.php
+++ wp-content/themes/cvtech/footer.php
@@ -12,7 +12,55 @@
 ?>
 <!-- footer -->
 </div><!-- #page -->
+<footer>
 
+        <div class="contenu-footer">
+
+            <div class="bloc footer-services">
+                <h3>Liens rapides</h3>
+                <ul class="liste-services">
+                    <li><a href="#">Créer son CV</a></li>
+                    <li><a href="#">Nos formations </a></li>
+                    <li><a href="#">F.A.Q</a></li>
+                    <li><a href="#">Politique de confidentialité </a></li>
+                    
+                </ul>
+            </div>
+
+            <div class="bloc footer-contact">
+                <h3>Restons en contact</h3>
+                <p>55-55-55-55-55</p>
+                <p>supportclient@contact.com</p>
+                <p>XX rue de la république, 76000 Rouen</p>
+            </div>
+
+            <div class="bloc footer-articles">
+                <h3> Nos derniers articles </h3>
+                <ul class="liste-articles">
+        
+                    <li> <a href=""> Lorem ipsum dolor sit amet ... </a></li>
+                    <li> <a href=""> Lorem ipsum dolor sit amet ... </a></li>
+                    <li> <a href=""> Lorem ipsum dolor sit amet ... </a></li>
+                    <li> <a href=""> Lorem ipsum dolor sit amet ... </a></li>
+                    <li> <a href=""> Lorem ipsum dolor sit amet ... </a></li>
+                        
+                    
+                </ul>
+            </div>
+
+            <div class="bloc footer-medias">
+                <h3>Nos réseaux</h3>
+                <ul class="liste-media">
+                    <li><a href="#" class="fa fa-facebook"></a></li>
+                    <li><a href="#" class="fa fa-instagram"></a></li>
+                    <li><a href="#" class="fa fa-twitter"></a></li>
+                    <li><a href="#" class="fa fa-youtube"></a></li>
+                </ul>
+            </div>
+          
+        </div>
+    
+    </footer>
 <script src="https://kit.fontawesome.com/6cb0bec8ab.js" crossorigin="anonymous"></script>
 <?php wp_footer(); ?>
 
